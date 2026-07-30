c2601893c7cc10047a6dc6a0ca835e4025b5a607
Tomari <tomari7878@gmail.com>
2026-07-30 08:43:50 -0500

    intergrated loaded variable

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 
 function Dictionary() {
-    let [keyword, setKeyword] = useState("Gauche");
+    let [keyword, setKeyword] = useState("Serendipity");
     let [results, setResults] = useState(null);
     let [photos, setPhotos] = useState(null);
     let [loaded, setLoaded] = useState(false);
@@ -17,20 +17,31 @@ function Dictionary() {
       setPhotos(response.data.photos);
     }
 
+    function search() {
+      let apiKey = `d723abdbd1batbf0d4fo3fa95586fbba`
+      let apiURL = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
+      let imgsURL = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`
+      axios.get(apiURL).then(handleResponse);
+      axios.get(imgsURL).then(handleImgsResponse)
+
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
-        let apiKey = `d723abdbd1batbf0d4fo3fa95586fbba`
-        let apiURL = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
-        let imgsURL = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`
-        axios.get(apiURL).then(handleResponse);
-        axios.get(imgsURL).then(handleImgsResponse)
+        search();
     }
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
     }
 
-    return (
+    function load() {
+      setLoaded(true);
+      search();
+    }
+
+    if (loaded) {   
+      return (
       <div className="Dictionary">
           <form onSubmit={handleSubmit} className="search-form">
             <input
@@ -45,7 +56,10 @@ function Dictionary() {
           </form>
           <Results results={results} photos={photos} />
       </div>
-    )
+    )} else {
+      load()
+      return "Loading..."
+    }
 }
 
 
